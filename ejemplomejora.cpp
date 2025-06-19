86233969e8b9473c9c4ee0a15a07d7af7fd0803e
javier14-dev <jcondorich@unjbg.edu.pe>
2025-06-18 21:33:15 -0500

    Agrega ejemplo culminado: REGISTRO DEL EMPLEADO

---
+++ ejemplomejora.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+struct EMP{
+    int num;
+    string nom;
+    float ven[12], sal;
+};
+
+int main(){
+    EMP EMPLEADOS[100];
+    int n;
+    cin>>n;
+    for(int i=0; i<n; i++){
+        cin>>EMPLEADOS[i].num;
+        cin.ignore();
+        getline(cin,EMPLEADOS[i].nom);
+        for(int j=0; j<=11; j++ )
+            cin>>EMPLEADOS[i].ven[j];
+        cin>>EMPLEADOS[i].sal;
+    } 
+
+    for(int i=0; i<n; i++){
+        cout<<EMPLEADOS[i].num<<endl;
+        cout<<EMPLEADOS[i].nom<<endl;
+        for(int j=0; j<12; j++)
+            cout<<EMPLEADOS[i].ven[j]<<" ";
+        cout<<"\n";
+        cout<<EMPLEADOS[i].sal<<endl<<endl;
+    }
+    return 0;
+}
